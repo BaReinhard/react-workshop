0592a81db554823f7fd03bc02a422a16a69abb7e
Brett Reinhard <Brett@Bretts-MacBook-Pro-2.local>
2017-07-15 17:10:11 -0700

    Testing Async with React

--- src/components/home/homePage.js
+++ src/components/home/homePage.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ContactList from '../contact/contactList';
+let newTitle = '';
 class HomePageComponent extends React.Component{
+    
     constructor(state,context){
         super(state,context);
+    this.state = {
+        title:''
+    };
+    }
+    fetchTitle=()=>{
+        let np = new Promise((resolve,reject)=>{
+            fetch("https://api.github.com/users/bareinhard").then(function(response){
+return response.json()
+}).then(function(nr){
+    newTitle= nr.bio
+    resolve(nr.bio);
+})
+        });
+    }
+        
+
+    getTitle=()=>{
+        fetch("https://api.github.com/users/bareinhard").then(function(response){
+return response.json()
+}).then(function(nr){
+    newTitle= nr.bio
+}).then(()=>{
+
+        this.setState({title:newTitle});
+        });
+
         
     }
-    
     render(){
         return(
             <div className="app-container">
-                <h1> Welcom to Brett's Contact App</h1>
+                <h1>{this.state.title}{this.getTitle()}</h1>
             </div>
         );
     }   
